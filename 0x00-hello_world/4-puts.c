997c8116fc81c74f30bfe04389d19cf19857001e
Victor Mutuma <vic8197tor@gmail.com>
2022-07-03 11:50:48 -0700

    Puts commande

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*printing using the puts command */
+/*printing using the puts command through main */
 
 int main(void)
 {
-	char puzzle[] = "Programming is like building a multilingual puzzle";
+	char puzzle[] = "\""Programming is like building a multilingual puzzle";
 
 	puts(puzzle);
 
