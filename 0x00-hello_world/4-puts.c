7577b72eb4a5f19aef8319987c6c37e56a330d03
Victor Mutuma <vic8197tor@gmail.com>
2022-07-03 11:55:04 -0700

    Puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	char puzzle[] =("\"Programming is like building a multilingual puzzle");
+	char puzzle[] = ("\"Programming is like building a multilingual puzzle");
 
 	puts(puzzle);
 
