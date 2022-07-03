4279bb1c5de16a0149b694890f62d2077666c6dc
Victor Mutuma <vic8197tor@gmail.com>
2022-07-03 12:27:23 -0700

    Printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -is void
+ * printing command
+ *
+ * Return: should give zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
