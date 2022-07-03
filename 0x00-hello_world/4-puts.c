52a5d90878c89f9a948825b88e4ff5f56c38d8ca
Victor Mutuma <vic8197tor@gmail.com>
2022-07-03 11:57:53 -0700

    Puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/*printing using the puts command through main */
+/*printing using the puts command through main
+ *main is the starting point
+ *return should be zero
+ * */
 
 int main(void)
 {
