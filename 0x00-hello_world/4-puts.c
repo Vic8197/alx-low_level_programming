d1b7248c6758f22144c361e7b0a3d8ca3617b8a9
Victor Mutuma <vic8197tor@gmail.com>
2022-07-03 11:46:49 -0700

    Print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*printing using the puts command */
+
+int main(void)
+{
+	char puzzle[] = "Programming is like building a multilingual puzzle";
+
+	puts(puzzle);
+
+	return (0);
+}
