8929e1f4a6e8a82d43a117fe19e43daaba18e5fc
Victor Mutuma <vic8197tor@gmail.com>
2022-07-11 07:46:08 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * numberz
+ * Return: zero
+ **/
+
+int main(void)
+
+{
+	int nu;
+
+	for (nu = 0; nu < 10; nu++)
+		putchar(nu + '0');
+
+putchar('\n');
+
+return (0);
+}
