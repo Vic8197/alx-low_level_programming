b079179c3e89281bc5fa6ba069a96ec5e05226d0
Victor Mutuma <vic8197tor@gmail.com>
2022-07-10 18:02:53 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * numbers
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int nu;
+
+	for (nu = 0; nu < 10; nu++)
+		printf("%i", nu);
+putchar('\n');
+return (0);
+}
