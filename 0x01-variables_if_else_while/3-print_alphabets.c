595ccfe3870794c612d3b927de27c194a20a1d11
Victor Mutuma <vic8197tor@gmail.com>
2022-07-10 17:00:32 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry pt
+ * print uppercase and lowercase
+ * Return: zero
+ */
+int main (void)
+{
+	char q;
+
+	for (q = 'a' ; q = 'z' ; q++)
+		putchar(q);
+	for (q = 'A' ; q = 'Z' ; q++)
+		putchar(q);
+	putchar('\n');
+	return (0);
+}
