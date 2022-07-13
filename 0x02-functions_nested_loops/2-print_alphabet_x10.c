49cfba39c77ed5cc5890629e07baa684f69d8678
Victor Mutuma <vic8197tor@gmail.com>
2022-07-13 14:31:57 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: zero
+ */
+
+void print_alphabet(void)
+{
+	int alphabet
+	int count
+
+	count = 0;
+	while (count > 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		count++;
+
+_putchar('\n');
+	}
+
+}
